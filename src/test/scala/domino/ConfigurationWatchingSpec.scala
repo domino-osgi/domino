1a8d7cc2b70d856e330717641daf0a49b1b681c1
Tobias Roeser <le.petit.fou@web.de>
2016-02-03 21:49:55 +0100

    More tests

--- src/test/scala/domino/ConfigurationWatchingSpec.scala
+++ src/test/scala/domino/ConfigurationWatchingSpec.scala
@@ -5,16 +5,14 @@ import org.osgi.service.cm.ManagedService
 import org.osgi.service.cm.ManagedServiceFactory
 import org.scalatest.Matchers
 import org.scalatest.WordSpecLike
-
 import domino.test.PojoSrTestHelper
-
 import domino.scala_osgi_metatype.builders.ObjectClass
 import org.osgi.framework.BundleContext
 import org.apache.felix.cm.impl.ConfigurationManager
 import org.osgi.service.cm.ConfigurationAdmin
 import scala.collection.mutable
-
 import scala.collection.JavaConverters._
+import org.osgi.framework.BundleActivator
 
 object ConfigurationWatchingSpec {
   class Log {
@@ -132,7 +130,7 @@ class ConfigurationWatchingSpec
       }
     }
 
-    "work with factory configurations" in {
+    def testFactoryConfigurations(f: Log => BundleActivator): Unit = {
       withPojoServiceRegistry { sr =>
         def bundleContext = sr.getBundleContext()
         val log = new Log
@@ -143,17 +141,7 @@ class ConfigurationWatchingSpec
         val ref = bundleContext.getServiceReference(classOf[ConfigurationAdmin])
         val ca: ConfigurationAdmin = bundleContext.getService(ref)
 
-        val activator = new DominoActivator {
-          whenBundleActive {
-            val reg: ServiceRegistration[ManagedServiceFactory] = whenFactoryConfigurationActive("domino.test", "Test") {
-              (conf: Map[String, Any], pid: String) =>
-                log.log("config: " + conf.map(c => c._1 + "=" + c._2).toList.sorted)
-                onStop {
-                  log.log("stop config: " + conf.map(c => c._1 + "=" + c._2).toList.sorted)
-                }
-            }
-          }
-        }
+        val activator = f(log)
         activator.start(bundleContext)
         assert(log.log === List())
 
@@ -192,7 +180,7 @@ class ConfigurationWatchingSpec
           case (msg, expected) =>
             msg should startWith(expected)
         }
-        
+
         // delete on config
         config1.delete()
         // expect no new config update
@@ -209,22 +197,32 @@ class ConfigurationWatchingSpec
       }
     }
 
-    "work with factory configurations and metatypes" in {
-      withPojoServiceRegistry { sr =>
-        val log = new Log
-        val activator = new DominoActivator {
-          whenBundleActive {
-            val reg: ServiceRegistration[ManagedServiceFactory] = whenFactoryConfigurationActive(objectClass) {
-              (conf: Map[String, Any], pid: String) =>
-                log.log("pid: " + pid + " config: " + conf)
-            }
+    "work with factory configurations" in {
+      testFactoryConfigurations(log => new DominoActivator {
+        whenBundleActive {
+          val reg: ServiceRegistration[ManagedServiceFactory] = whenFactoryConfigurationActive("domino.test", "Test") {
+            (conf: Map[String, Any], pid: String) =>
+              log.log("config: " + conf.map(c => c._1 + "=" + c._2).toList.sorted)
+              onStop {
+                log.log("stop config: " + conf.map(c => c._1 + "=" + c._2).toList.sorted)
+              }
           }
         }
-        activator.start(sr.getBundleContext)
-        assert(log.log === List())
-        // TODO: check with ConfigAdmin
-        pending
-      }
+      })
+    }
+
+    "work with factory configurations and metatypes" in {
+      testFactoryConfigurations(log => new DominoActivator {
+        whenBundleActive {
+          val reg: ServiceRegistration[ManagedServiceFactory] = whenFactoryConfigurationActive(objectClass) {
+            (conf: Map[String, Any], pid: String) =>
+              log.log("config: " + conf.map(c => c._1 + "=" + c._2).toList.sorted)
+              onStop {
+                log.log("stop config: " + conf.map(c => c._1 + "=" + c._2).toList.sorted)
+              }
+          }
+        }
+      })
     }
 
   }
