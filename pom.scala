bfd34562a840dad4e5bfe1657232424e7876010b
Tobias Roeser <le.petit.fou@web.de>
2017-12-01 13:29:27 +0100

    Added optional logging dependencies in prep for internal logger

--- pom.scala
+++ pom.scala
@@ -1,27 +1,27 @@
 import org.sonatype.maven.polyglot.scala.model._
 import scala.collection.immutable._
 
-val dominoVersion = "1.1.3-SNAPSHOT"
-
-implicit val scalaVersion = System.getenv("SCALA_VERSION") match {
+// Scala setup, you can specify the scala version via env variable SCALA_VERSION
+implicit val scalaVersion: ScalaVersion = System.getenv("SCALA_VERSION") match {
   case null => ScalaVersion("2.12.3")
   case v => ScalaVersion(v)
 }
 println("Using Scala version: " + scalaVersion.version)
 
+val dominoVersion = "1.1.3-SNAPSHOT"
 val url = "https://github.com/domino-osgi/domino"
 
 object Deps {
-  // compile dependencies
   val scalaLibrary = "org.scala-lang" % "scala-library" % scalaVersion.version
   val scalaReflect = "org.scala-lang" % "scala-reflect" % scalaVersion.version
   val osgiCore = "org.osgi" % "org.osgi.core" % "4.3.0"
   val osgiCompendium = "org.osgi" % "org.osgi.compendium" % "4.3.0"
+  val slf4j = "org.slf4j" % "slf4j-api" % "1.7.25"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
   val felixConfigAdmin = "org.apache.felix" % "org.apache.felix.configadmin" % "1.8.8"
   val pojosr = "com.googlecode.pojosr" % "de.kalpatec.pojosr.framework.bare" % "0.2.1"
-  // val pojosr = "com.googlecode.pojosr" % "de.kalpatec.pojosr.framework.bare" % "0.3.0-SNAPSHOT"
   val bndlib = "biz.aQute.bnd" % "biz.aQute.bndlib" % "3.5.0"
+  val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.1.3"
 }
 
 object Plugins {
@@ -75,10 +75,12 @@ Model(
     Deps.scalaReflect,
     Deps.osgiCore,
     Deps.osgiCompendium,
+    Dependency(Deps.slf4j, optional = true),
     // test dependencies
     Deps.scalaTest % "test",
     Deps.felixConfigAdmin % "test",
-    Deps.pojosr % "test"
+    Deps.pojosr % "test",
+    Deps.logbackClassic % "test"
   ),
   build = Build(
     outputDirectory = "${project.build.directory}/classes_" + scalaVersion.binaryVersion,
@@ -97,6 +99,7 @@ Model(
       )
     ),
     plugins = Seq(
+      // Build OSGi Manifest and bundle, also check version compatibility (baselining)
       Plugin(
         Plugins.bundle,
         dependencies = Seq(
@@ -110,12 +113,14 @@ Model(
         ),
         executions = Seq(Execution(phase = "verify", goals = Seq("baseline")))
       ),
+      // Skip default Maven tests
       Plugin(
         Plugins.surefire,
         configuration = Config(
           skipTests = true
         )
       ),
+      // Run tests with Scalatest
       Plugin(
         Plugins.scalatest,
         executions = Seq(Execution(id = "test", goals = Seq("test"))),
@@ -124,6 +129,7 @@ Model(
           junitxml = "."
         )
       ),
+      // Compile Scala code
       Plugin(
         Plugins.scala,
         executions = Seq(Execution(goals = Seq("add-source", "compile", "testCompile"))),
